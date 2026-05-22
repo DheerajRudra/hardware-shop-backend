e882679c1fa81911cd163cfcdfd43da4f8b8eb1b
DheerajRudra <dheeraj2023rudra@gmail.com>
2026-05-22 17:12:39 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -6,7 +6,9 @@ const ProductSchema = new mongoose.Schema({
   actualPrice:  { type: Number, required: true },
   sellingPrice: { type: Number, required: true },
   stock:        { type: Number, default: 0 },
-  alert:        { type: Number, default: 5 }
+  alert:        { type: Number, default: 5 },
+  isDeleted:    { type: Boolean, default: false },
+  deletedAt:    { type: Date, default: null }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', ProductSchema);
