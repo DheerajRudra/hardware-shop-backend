3825532b06b2590866fef027c60979f19eb5bdfe
DheerajRudra <dheeraj2023rudra@gmail.com>
2026-05-22 16:12:28 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Product = require('./Product');
+const Product = require('../models/Product');
 
 // GET all products
 router.get('/', async (req, res) => {
