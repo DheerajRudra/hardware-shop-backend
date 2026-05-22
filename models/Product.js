89da67529c2b17db9022c449d0d2e702518e8383
DheerajRudra <dheeraj2023rudra@gmail.com>
2026-05-22 16:37:38 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  name:     { type: String, required: true },
-  category: { type: String, default: 'Other' },
-  price:    { type: Number, required: true },
-  stock:    { type: Number, default: 0 },
-  alert:    { type: Number, default: 5 }
+  name:         { type: String, required: true },
+  category:     { type: String, default: 'Other' },
+  actualPrice:  { type: Number, required: true },
+  sellingPrice: { type: Number, required: true },
+  stock:        { type: Number, default: 0 },
+  alert:        { type: Number, default: 5 }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', ProductSchema);
